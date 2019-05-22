5e9f426a3e91758c2d2c8d565cc429808377c872
malydok <marek@devalchemist.com>
2019-05-22 23:03:49 +0200

    init hapi test

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import * as Crypto from 'crypto';
+import * as Hapi from 'typesafe-hapi';
+
+interface Room {
+  id: string;
+  players: string[];
+}
+const rooms: Room[] = [];
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/createRoom',
+    handler(request, h) {
+      const roomId = Crypto.createHash('sha1').digest('hex');
+      const newRoom = {
+        id: roomId,
+        players: [],
+      };
+      rooms.push(newRoom);
+      console.log('New room created', newRoom);
+      return h.redirect(`/room/${roomId}`);
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/room/{roomId}',
+    handler(request, h) {
+      const roomId = request.params.roomId;
+      const possibleRoom = rooms.find(room => room.id === roomId);
+      if (possibleRoom) {
+        possibleRoom.players.push('player1');
+        return JSON.stringify(possibleRoom);
+      }
+      return 'No such room!';
+    },
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
