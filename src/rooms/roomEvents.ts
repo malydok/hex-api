1f45d8666ed9e8c6cd8b9fcaa34c5994b8b4d909
devalche <marek@devalchemist.com>
2020-01-30 17:36:11 +0100

    refactor events

--- src/rooms/roomEvents.ts
+++ src/rooms/roomEvents.ts
@@ -1,33 +1,20 @@
 import { RoomDigest, Room } from './room.types';
 import { Game } from '../game/game.types';
 
-export const ROOMS_SETUP = (rooms: RoomDigest[]) => ({
-  type: 'ROOMS_SETUP',
-  payload: rooms,
-});
-export const ROOMS_CHANGE = (rooms: RoomDigest[]) => ({
-  type: 'ROOMS_CHANGE',
-  payload: rooms,
-});
-
-export const ROOM_FULL = () => ({
-  type: 'ROOM_FULL',
-});
-export const ROOM_SETUP = (room: Room) => ({
-  type: 'ROOM_SETUP',
-  payload: room,
-});
-export const ROOM_UPDATE = (room: Room) => ({
-  type: 'ROOM_UPDATE',
-  payload: room,
-});
-
-export const CHAT_MESSAGE = (payload: { message: string; client: string }) => ({
-  type: 'CHAT_MESSAGE',
+const createEvent = <T>(name: string) => (payload?: T) => ({
+  type: name,
   payload,
 });
 
-export const GAME_UPDATE = (payload: Game) => ({
-  type: 'GAME_UPDATE',
-  payload,
-});
+export const ROOMS_SETUP = createEvent<RoomDigest[]>('ROOMS_SETUP');
+export const ROOMS_CHANGE = createEvent<RoomDigest[]>('ROOMS_CHANGE');
+
+export const ROOM_FULL = createEvent('ROOM_FULL');
+export const ROOM_SETUP = createEvent<Room>('ROOM_SETUP');
+export const ROOM_UPDATE = createEvent<Room>('ROOM_UPDATE');
+
+export const CHAT_MESSAGE = createEvent<{ message: string; client: string }>(
+  'CHAT_MESSAGE',
+);
+
+export const GAME_UPDATE = createEvent<Game>('GAME_UPDATE');
